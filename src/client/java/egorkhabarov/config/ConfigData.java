a53c2867923cf2167700da7e9f2203cc10432485
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-10-25 04:17:10 +0400

    update

--- src/client/java/egorkhabarov/config/ConfigData.java
+++ src/client/java/egorkhabarov/config/ConfigData.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class ConfigData {
     public boolean enabled = false;
     public boolean auto_finder_enabled = false;
-    public String comment;
     public long scan_interval_ms = 1000;
     public double scan_radius = 2.0;
     public long villager_cache_ttl = 5000;
