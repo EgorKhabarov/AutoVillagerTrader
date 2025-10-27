62c3997844a4235a2ead7415a342714ae9e1b5c6
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-10-28 01:13:37 +0400

    updated Condition

--- src/client/java/egorkhabarov/config/Condition.java
+++ src/client/java/egorkhabarov/config/Condition.java
@@ -12,6 +12,7 @@ public class Condition {
     private static String translate(String condition) {
         return switch (condition) {
             case "=" -> "eq";
+            case "==" -> "eq";
             case ">" -> "mt";
             case "<" -> "lt";
             case ">=" -> "me";
